77a0bf5c3196b6c73702ccbd96df57c2005fc992
elaradi23 <ta.elaradi23@gmail.com>
2018-07-09 20:55:05 +0000

    Added comments for clarification

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,7 @@ var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Take argv as inputs for repoOwner and repoName
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
@@ -16,18 +17,21 @@ function getRepoContributors(repoOwner, repoName, cb){
     }
   };
 
+  //The body is pulled, parsed as a JSON object and then passed to the callback function
   request(options, function(err, res, body){
     var bodyJSON = JSON.parse(body);
     cb(err, bodyJSON);
   });
 }
 
+//Check if both repoOwner and repoName are given, else do not run getRepoContributors
 if(!repoOwner || !repoName){
   console.log('Error: No input for repo-owner or repo-name!');
 } else {
   getRepoContributors(repoOwner, repoName, function(err, result) {
     console.log("Errors:", err);
     for(var item in result){
+      //The file path will use the 'login' field as a name for the image file
       downloadImageByURL(result[item].avatar_url, result[item].login);
     }
 
